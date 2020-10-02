ba4878b637847f86058989d34a8ef5acfb1e8e45
ESE <tofikstamboli@gmail.com>
2020-10-02 13:09:00 +0530

    Approved by Tofik

--- app/src/main/java/com/example/gamesbag/HomeActivity.java
+++ app/src/main/java/com/example/gamesbag/HomeActivity.java
@@ -38,7 +38,7 @@ public class HomeActivity extends Activity {
 
     private void initRecyclerView() {
 //New line added by sonal
-//trialBySonal
+//trialBySonal --> Approved by Tofik
         //Create dummy data for RecyclerView
         int listSize = 50;
         int ITEM = 0;
